328db6926ca906fd5099d6edaa3db4e3af4c928b
knapen <knapen@35ae2efa-3d17-4eea-affb-b6c2657e1a69>
2010-11-28 17:49:31 +0000

    added retinotopic remapping session class
    git-svn-id: svn+ssh://localhost/var/lab/svn/analysis_tools@11 35ae2efa-3d17-4eea-affb-b6c2657e1a69

---
+++ Tools/Sessions/RetinotopicRemappingSession.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Session.py
+
+Created by Tomas HJ Knapen on 2009-11-26.
+Copyright (c) 2009 TK. All rights reserved.
+"""
+
+import * from Session
+import * from RetinotopicMappingSession
+
+class RetinotopicRemappingSession(RetinotopicMappingSession):
+	def runQC(self, rois = ['V1','V2','V3']):
+		"""
+		Quality control for this session would mean to have a look at SNR in different areas
+		runQC assumes a list of masks is in place in the processed/mri/masks folder and runs separate analyses for each of these ROIs
+		"""
+		fs = 10
+		
+		if len(self.conditionDict['polar']) > 0:
+			# polar files
+			rawInputFileNames = [self.runFile( stage = 'processed/mri', run = self.runList[pC], postFix = ['mcf']) for pC in self.scanTypeDict['epi_bold']]
+			distilledInputFileNames = [os.path.join(self.runFolder(stage = 'processed/mri', run = self.runList[pC]), self.runList[pC].condition) for pC in self.scanTypeDict['epi_bold']]
+			distilledInputFileNamesFull = [os.path.join(self.conditionFolder(stage = 'processed/mri', run = self.runList[self.conditionDict['polar'][0]]), 'polar'),os.path.join(self.conditionFolder(stage = 'processed/mri', run = self.runList[self.conditionDict['polar'][0]]), 'eccen')]
+			
+			for (pd, rd) in zip(distilledInputFileNames, rawInputFileNames):
+				# setting up the data files
+				rdIm = NiftiImage(rd)
+				pdIm = NiftiImage(pd)
+				
+				pp = PdfPages(self.runFile(stage = 'processed/mri', run = self.runList[self.scanTypeDict['epi_bold'][rawInputFileNames.index(rd)]], extension = '.pdf' ))
+				
+				for roi in rois:
+					for hemi in ['lh','rh']:
+						maskFileName = self.runFile(stage = 'processed/mri/masks', base = hemi + '.' + roi )
+						
+						# run directly on the results of retmapping, that is, the polar.nii.gz and eccen.nii.gz files 
+						# mask the input files:
+						imO = ImageMaskingOperator(pdIm, maskObject = maskFileName)
+						roiStatData = imO.applySingleMask(flat = True)		# F value is 3, noise SD is 6, phase value is 9
+						
+						fig = pl.figure()
+						s = fig.add_subplot(321)
+						pl.hist(roiStatData[3], color='r', alpha = 0.25, normed = True, bins = np.linspace(0,10,25), rwidth = 0.5 )
+						s.set_title('F values in ' + roi + ' ' + hemi + '\n' + str(rd.split('/')[-3:]), fontsize=fs)
+						s.axis([0, 10, 0, 0.5])
+						s = fig.add_subplot(323)
+						pl.hist(roiStatData[9], color='g', alpha = 0.25, normed = True, bins = np.linspace(-pi,pi,25), rwidth = 0.5)
+						s.set_title('phase values in ' + roi, fontsize=fs)
+						s.axis([-pi, pi, 0, 0.5])
+						s = fig.add_subplot(325)
+						pl.hist(roiStatData[6], color='b', alpha = 0.25, normed = True, bins = np.linspace(50,250,25), rwidth = 0.5)
+						s.set_title('noise SD values in ' + roi, fontsize=fs)
+						s.axis([50, 250, 0, 0.02])
+						
+						# run on the raw periodic data, that is, the polar.nii.gz and eccen.nii.gz files 
+						imO = ImageMaskingOperator(rdIm, maskObject = maskFileName)
+						roiRawData = imO.applySingleMask(flat = True)
+						self.logger.debug('masked raw data from roi %s in hemi %s is %s', roi, hemi, str(roiRawData.shape) )
+						# the best voxel in this file:
+						bestVoxArray = roiStatData[3] == roiStatData[3].max()
+						# show timecourse of run in this best voxel
+						s = fig.add_subplot(222)
+						s.plot( np.arange(rdIm.data.shape[0]) * rdIm.rtime, roiRawData[:,bestVoxArray] )
+						s.set_title('best voxel time course in ' + roi + ' ' + hemi + '\n' + str(rd.split('/')[-3:]), fontsize=fs)
+						pl.xlabel('time [s]')
+						s = fig.add_subplot(224)
+						ftRoiRD = np.abs(np.fft.fft(roiRawData, axis = 0)).mean(axis = -1)
+						ftticks = np.fft.fftfreq(roiRawData.shape[0], d = rdIm.rtime )
+						s.set_title('power spectrum ' + roi + ' ' + hemi, fontsize=fs)
+						s.plot( ftticks[1:floor(ftRoiRD.shape[0]/2.0)], ftRoiRD[1:floor(ftRoiRD.shape[0]/2.0)] )
+						pl.xlabel('frequency [Hz]')
+					
+						pp.savefig()
+					
+				pp.close()
+#		pl.show()
+	
