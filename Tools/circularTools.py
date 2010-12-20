41d0366db735c9fdef1d89ca7fe9df93da2ea1ba
knapen <knapen@35ae2efa-3d17-4eea-affb-b6c2657e1a69>
2010-12-20 21:05:21 +0000

    added circular data functions
    git-svn-id: svn+ssh://localhost/var/lab/svn/analysis_tools@46 35ae2efa-3d17-4eea-affb-b6c2657e1a69

--- Tools/circularTools.py
+++ Tools/circularTools.py
@@ -12,34 +12,35 @@ import numpy as np
 import matplotlib.pylab as pl
 from math import *
 
+def positivePhases( phases ):
+	return fmod(phases + 2 * pi, 2 * pi)
 
-def phaseDifference( phase1, phase2 ):
+def circularDifference( phase1, phase2 ):
 	# we need to do a circular subtraction; as follows
 	return np.fmod(phase1 - phase2 + 3*pi, 2*pi) - pi
 
-def phaseStandardDeviation( real, imag, noiseSD ):
-	"""
-	# so, for each of these phaseArrays, we calculate not only the phase, but also,
-	# following Gudbjartsson and Patz, 1995, eq. 8
-	# the standard deviation on the phase, which is equal to 
-	# the standard deviation of the noise divided by the amplitude of the signal, 
-	# and is gaussian distributed.
-	"""
-	complexAtFundamental = np.zeros(np.shape(real), dtype = np.complex128)
-	complexAtFundamental.real = real
-	complexAtFundamental.imag = imag
-	amplitudes = abs(complexAtFundamental)
-	result = noiseSD/amplitudes
-	# take out places where amplitude was zero, i.e. rule above resulted in inf.
-	# these are replaced by the sdForZeroAmplitude defined above
-	resultInfToZero = np.nan_to_num(result * np.isfinite(result))
-	return np.clip( np.nan_to_num( np.isinf( result ) * result ), 0, sdForZeroAmplitude ) + resultInfToZero
+def SCR( phases ):
+	S,C = np.sin(phases).sum(), np.cos(phases).sum()
+	R = sqrt(S**2+C**2)
+	return S, C, R
+
+def circularMean( phases ):
+	S, C, R = SCR(phases)
+	return np.arcsin(S/R)
+
+def circularVariance( phases ):
+	S, C, R = SCR(phases)
+	return 1 - (R/phases.shape[0])
+
+def circularStandardDeviation( phases ):
+	V = circularVariance(phases)
+	return sqrt( -2.0 * log(1.0 - V) )
 
 def standardDeviationOnPhaseDifferences( sigma1, sigma2 ):
 	# gaussian distributions allow us to easily get the standard deviation of the difference between two of them.
 	return np.sqrt( np.power(sigma1, 2) + np.power(sigma2, 2) )
 
-def phaseCorrelation( phase1, phase2 ):
+def circularCorrelation( phase1, phase2 ):
 	"""
 	phaseCorrelation calculates the circular correlation coefficient 
 	according to Fisher and Lee '83
@@ -57,3 +58,21 @@ def phaseCorrelation( phase1, phase2 ):
 	denom = sqrt(denomA * denomB)
 	r = enum / denom
 	return r
+
+def circularStandardDeviationFromNoiseSD( real, imag, noiseSD ):
+	"""
+	# so, for each of these phaseArrays, we calculate not only the phase, but also,
+	# following Gudbjartsson and Patz, 1995, eq. 8
+	# the standard deviation on the phase, which is equal to 
+	# the standard deviation of the noise divided by the amplitude of the signal, 
+	# and is gaussian distributed.
+	"""
+	complexAtFundamental = np.zeros(np.shape(real), dtype = np.complex128)
+	complexAtFundamental.real = real
+	complexAtFundamental.imag = imag
+	amplitudes = abs(complexAtFundamental)
+	result = noiseSD/amplitudes
+	# take out places where amplitude was zero, i.e. rule above resulted in inf.
+	# these are replaced by the sdForZeroAmplitude defined above
+	resultInfToZero = np.nan_to_num(result * np.isfinite(result))
+	return np.clip( np.nan_to_num( np.isinf( result ) * result ), 0, sdForZeroAmplitude ) + resultInfToZero
