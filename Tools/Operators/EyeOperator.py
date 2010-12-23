507b5e45a7ae39f884f975125afed0c32cc90c55
knapen <knapen@35ae2efa-3d17-4eea-affb-b6c2657e1a69>
2010-12-23 15:10:38 +0000

    started prelim eyelink analysis inclusion
    git-svn-id: svn+ssh://localhost/var/lab/svn/analysis_tools@48 35ae2efa-3d17-4eea-affb-b6c2657e1a69

--- Tools/Operators/EyeOperator.py
+++ Tools/Operators/EyeOperator.py
@@ -28,18 +28,24 @@ class EyeOperator( Operator ):
 		EyeOperator operator takes a filename
 		"""
 		super(EyeOperator, self).__init__(inputObject = inputObject, **kwargs)
-		# convert inputObject to NiftiImage while generating inputFileName from it, if necessary.
 		if self.inputObject.__class__.__name__ == 'str':
 			self.inputFileName = self.inputObject
-			dataFile = loadmat(self.inputObject)['dataEYD'][0,0]
-			self.inputObject = dataFile
 		self.logger.info('started with ' +os.path.split(self.inputFileName)[-1])
 		
+
+class ASLEyeOperator( EyeOperator ):
+	"""docstring for ASLEyeOperator"""
+	def __init__(self, inputObject, **kwargs):
+		super(ASLEyeOperator, self).__init__(inputObject = inputObject, **kwargs)
+		self.type = 'ASL'
+		self.rawDataFile = loadmat(self.inputFileName)['dataEYD'][0,0]
+		
+
 	def trSignals(self):
-		self.TRinfo = (np.array(self.inputObject['XDAT']-np.min(self.inputObject['XDAT']))/(np.max(self.inputObject['XDAT'])-np.min(self.inputObject['XDAT'])) == 1.0).ravel()
+		self.TRinfo = (np.array(self.rawDataFile['XDAT']-np.min(self.rawDataFile['XDAT']))/(np.max(self.rawDataFile['XDAT'])-np.min(self.rawDataFile['XDAT'])) == 1.0).ravel()
 		# take even non-same consecutive TR samples
 		self.TRtimeIndices = np.arange(self.TRinfo.shape[0])[self.TRinfo[:-1,0]==self.TRinfo[1:,0]][::2]
-		self.TRtimes = self.inputObject['time'][self.TRtimeIndices]
+		self.TRtimes = self.rawDataFile['time'][self.TRtimeIndices]
 		self.firstTR = {'index': self.TRtimeIndices[0], 'time':self.TRTimes[0]}
 		self.TR = self.TRtimes[1]-self.TRtimes[0]
 		
@@ -49,8 +55,34 @@ class EyeOperator( Operator ):
 		# analyze incoming TR signals
 		self.trSignals()
 		
-		self.gazeData = self.inputObject['horz_gaze_coord'][self.firstTRSample:self.firstTRSample + sampleFrequency * self.TR * self.nrVolumes ]
+		self.gazeData = self.rawDataFile['horz_gaze_coord'][self.firstTRSample:self.firstTRSample + sampleFrequency * self.TR * self.nrVolumes ]
 		self.gazeData = self.gazeData.reshape(self.gazeData.shape[0]/(sampleFrequency * self.TR), sampleFrequency * self.TR).transpose()
-		self.blinks = self.inputObject['pupil_recogn'][self.firstTRSample: self.firstTRSample + self.TR * self.nrVolumes ]
+		self.blinks = self.rawDataFile['pupil_recogn'][self.firstTRSample: self.firstTRSample + self.TR * self.nrVolumes ]
 		
 		
+class EyelinkOperator( EyeOperator ):
+	"""docstring for EyelinkOperator"""
+	def __init__(self, inputObject, **kwargs):
+		super(EyelinkOperator, self).__init__(inputObject = inputObject, **kwargs)
+		
+		if os.path.splitext(self.inputObject)[-1] == '.edf':
+			self.type = 'eyelink'
+			self.inputFileName = self.inputObject
+			# in Kwargs there's a variable that we can set to 
+			if hasattr(self, is_split):
+				self.messageFile = os.path.splitext(self.inputFileName)[0] + '.msg'
+				self.gazeFile = os.path.splitext(self.inputFileName)[0] + '.gaz'
+			else:
+				eac = EDF2ASCOperator(self.inputFileName)
+				eac.configure()
+				eac.execute()
+		else:
+			self.logger.warning('Input object is not an edf file')
+	
+	def loadData(self):
+		self.gazeData = np.loadtxt(self.gazeFile)
+		mF = open(self.messagefile, 'r')
+		self.msgData = mF.readlines()
+		mF.close()
+	
+	def splitToTrials
