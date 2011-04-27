cd65afc9d36a10c280df0515afc79e3f6aa54c4f
knapen <knapen@35ae2efa-3d17-4eea-affb-b6c2657e1a69>
2011-04-27 13:25:12 +0000

    Added an eyelink/psychophysics session class
    git-svn-id: svn+ssh://localhost/var/lab/svn/analysis_tools@93 35ae2efa-3d17-4eea-affb-b6c2657e1a69

---
+++ Tools/Sessions/EyeLinkSession.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+EyeLinkSession.py
+
+Created by Tomas Knapen on 2011-04-27.
+Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+"""
+
+import os, sys, pickle, math
+from subprocess import *
+
+import scipy as sp
+import numpy as np
+import matplotlib.pylab as pl
+from matplotlib.backends.backend_pdf import PdfPages
+from pypsignifit import *
+from tables import *
+
+import pp
+import logging, logging.handlers, logging.config
+
+from ..log import *
+from ..Run import *
+from ..Subjects.Subject import *
+from ..Operators.Operator import *
+from ..Operators.CommandLineOperator import *
+from ..Operators.ImageOperator import *
+from ..Operators.BehaviorOperator import *
+from ..Operators.ArrayOperator import *
+from ..Operators.EyeOperator import *
+
+class EyelinkSession(object):
+	def __init__(self, ID, subject, project_name, experiment_name, base_directory, wild_card, loggingLevel = logging.DEBUG):
+		self.ID = ID
+		self.subject = subject
+		self.project_name = project_name
+		self.experiment_name = experiment_name
+		self.wildcard = wildcard
+		try:
+			os.mkdir(os.path.join( base_directory, project_name, experiment_name, self.subject.initials ))
+		except OSError:
+			pass
+		self.base_directory = os.path.join( base_directory, project_name, experiment_name, self.subject.initials )
+		self.hdf5_filename = os.path.join(self.base_directory, 'processed', self.subject.initials + '.hdf5')
+		
+		self.create_folder_hierarchy()
+		
+		self.loggingLevel = loggingLevel
+		
+		# add logging for this session
+		# sessions create their own logging file handler
+		self.logger = logging.getLogger( self.__class__.__name__ )
+		self.logger.setLevel(self.loggingLevel)
+		addLoggingHandler( logging.handlers.TimedRotatingFileHandler( os.path.join(self.stageFolder(stage = 'processed/mri/log'), 'sessionLogFile.log'), when = 'H', delay = 2, backupCount = 10), loggingLevel = self.loggingLevel )
+		loggingLevelSetup()
+		for handler in logging_handlers:
+			self.logger.addHandler(handler)
+		self.logger.info('starting analysis of session ' + str(self.ID))
+		
+	def create_folder_hierarchy(self):
+		"""docstring for createFolderHierarchy"""
+		# check for basic directory
+		for p in ['raw','processed','figs','log']:
+			try:
+				os.mkdir(os.path.join(self.base_directory, p))
+			except OSError:
+				pass
+		
+	def import_raw_data(self, original_data_directory):
+		"""docstring for import_raw_data"""
+		os.chdir(original_data_directory)
+		behavior_files = subprocess.Popen('ls ' + self.wildcard + '*_outputDict.pickle', shell=True, stdout=subprocess.PIPE).communicate()[0].split('\n')[0:-1]
+		eye_files = [f.split('_outputDict.pickle') + '.edf' for f in behavior_files]
+		
+		# put output dicts and eyelink files next to one another - that's what the eyelinkoperator expects.
+		for i in range(len(behavior_files)):
+			ExecCommandLine('cp ' + behavior_files[i] + ' ' + os.path.join(self.base_directory, 'raw', behavior_files[i]) )
+			ExecCommandLine('cp ' + eye_files[i] + ' ' + os.path.join(self.base_directory, 'raw', eye_files[i]) )
+	
+	def convert_edf(self):
+		"""docstring for convert_edf"""
+		os.chdir(os.path.join(self.base_directory, 'raw'))
+		# what files are there to analyze?
+		edf_files = subprocess.Popen('ls ' + self.wildcard + '*.edf', shell=True, stdout=subprocess.PIPE).communicate()[0].split('\n')[0:-1]
+		msg_files = subprocess.Popen('ls ' + self.wildcard + '*.msg', shell=True, stdout=subprocess.PIPE).communicate()[0].split('\n')[0:-1]
+
+		edf_files_no_ext = [f.split('.edf')[0] for f in edf_files]
+		msg_files_no_ext = [f.split('.msg')[0] for f in msg_files]
+		
+		eyelink_fos = []
+		for f in edf_files_no_ext:
+			# check which files are already split - and get their data. 
+			# if they haven't been split, split then and then import their data
+			if f not in msg_files_no_ext:
+				elo = EyelinkOperator( inputObject = f+'.edf', split = True )
+				elo.loadData(get_gaze_data = True)
+				eyelink_fos.append(elo)
+			else:
+				elo = EyelinkOperator( inputObject = f+'.edf', split = False )
+				elo.loadData(get_gaze_data = True)
+				eyelink_fos.append(elo)
+
+			elo.findAll()
+			for r in range(len(elo.parameters)):
+				if 'answer' not in elo.parameters[r].keys():
+					print 'no answer in run # ' + elo.gazeFile + ' trial # ' + str(r)
+					elo.parameters.pop(r)
+		
+		order = np.argsort(np.array([int(elo.timeStamp.strftime("%Y%m%d%H%M%S")) for elo in eyelink_fos]))
+		for i in order:
+			eyelink_fos[i].processIntoTable(self.hdf5_filename, name = 'run_' + str(i))
+	
+	def import_behavior_data(self)
+		behavioral_data = []
+		h5f = openFile(self.hdf5_filename, mode = "r" )
+		for r in h5f.walkGroups(where = '/'):
+			behavioral_data.append(r.trial_parameters.read())
+		self.behavioral_data = np.concatenate(behavioral_data)
+	
+	
