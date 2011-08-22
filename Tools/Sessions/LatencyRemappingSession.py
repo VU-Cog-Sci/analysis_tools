26c7ed687d08819bb2e4aa3898a4c67d53d8d583
knapen <knapen@35ae2efa-3d17-4eea-affb-b6c2657e1a69>
2011-08-22 08:09:02 +0000

    started analysis of eye link data in the scanner
    git-svn-id: svn+ssh://localhost/var/lab/svn/analysis_tools@190 35ae2efa-3d17-4eea-affb-b6c2657e1a69

--- Tools/Sessions/LatencyRemappingSession.py
+++ Tools/Sessions/LatencyRemappingSession.py
@@ -8,10 +8,35 @@ Copyright (c) 2009 TK. All rights reserved.
 """
 
 from Session import * 
-from RetinotopicMappingSession import *
 from ..circularTools import *
 import matplotlib.cm as cm
 from pylab import *
 
 class LatencyRemappingSession(Session):
-	def b
+	def saccade_latency_analysis_all_runs(self):
+		self.mapper_saccade_data = []
+		for r in self.runList(self.conditionDict['Mapper']):
+			self.mapper_saccade_data.append(saccade_latency_analysis_one_run(r))
+		self.remapping_saccade_data = []
+		for r in self.runList(self.conditionDict['Remapping']):
+			self.remapping_saccade_data.append(saccade_latency_analysis_one_run(r))
+	
+	def parameter_analysis_all_runs(self):
+		self.mapper_parameter_data = []
+		for r in self.runList(self.conditionDict['Mapper']):
+			self.mapper_parameter_data.append(parameter_analysis_one_run(r))
+		self.remapping_saccade_data = []
+		for r in self.runList(self.conditionDict['Remapping']):
+			self.remapping_parameter_data.append(parameter_analysis_one_run(r))
+		
+	def saccade_latency_analysis_one_run(self, run):
+		elO = EyeLinkOperator(self.runFile(stage = 'processed/eye', run = r, extension = '.hdf5'))
+		el_saccades = elO.get_EL_events_per_trial(run_name = 'bla', trial_ranges = [[0,-1]], trial_phase_range = [2,4], data_type = 'saccades')
+		
+		
+		
+		
+	def parameter_analysis_one_run(self, run):
+		elO = EyeLinkOperator(self.runFile(stage = 'processed/eye', run = r, extension = '.hdf5'))
+		elO.import_parameters(run_name = 'bla')
+		
